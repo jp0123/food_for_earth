c3591cf947c48166b2a301bcc192ef6251d3250a
Joseph Park <joseph.thomas.park@gmail.com>
2018-08-04 16:36:30 +1000

    Update Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
-import NavBar from "./components/layout/NavBar";
-import Footer from "./components/layout/Footer";
-import Landing from "./components/layout/Landing";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/layout/navBar";
+import Footer from "./components/layout/footer";
+import Landing from "./components/layout/landing";
+import About from "./components/layout/about";
+import Login from "./components/auth/login";
+import Register from "./components/auth/register";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div>
+          <NavBar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
