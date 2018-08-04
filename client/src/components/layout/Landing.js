b226f8fdac3916466392e80be40306952a9149b8
Joseph Park <joseph.thomas.park@gmail.com>
2018-08-04 22:54:24 +1000

    Add CallToAction component

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./landing/hero";
+import CallToAction from "./landing/callToAction";
 
 export default () => {
   return (
     <div>
       <Hero />
+      <CallToAction />
     </div>
   );
 };
---
+++ client/src/components/layout/landing/callToAction.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Button, Row, Col, Container } from "reactstrap";
+import { Link } from "react-router-dom";
+
+const CallToAction = () => {
+  const title_user_1 = "Make Fertilizer";
+  const content_user_1 =
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, ipsam qui totam debitis autem saepe minus, excepturi doloribus facere molestiae error perferendis impedit sapiente in soluta? Minima quod quibusdam consectetur.";
+  const title_user_2 = "Receive Fertilizer";
+  const content_user_2 =
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, ipsam qui totam debitis autem saepe minus, excepturi doloribus facere molestiae error perferendis impedit sapiente in soluta? Minima quod quibusdam consectetur.";
+
+  return (
+    <Container>
+      <Row>
+        <Col sm="12" md="6">
+          <div className="images-container">
+            <img
+              className="img-fluid"
+              src="http://via.placeholder.com/300x300"
+              alt="Fertilizer producer"
+            />
+          </div>
+          <h4 className="text-style-1">{title_user_1}</h4>
+          <p className="text-style-2">{content_user_1}</p>
+          <Link to="/make-fertilizer">
+            <Button className="button-style-one">Details</Button>
+          </Link>
+        </Col>
+        <Col sm="12" md="6">
+          <div className="images-container">
+            <img
+              className="img-fluid"
+              src="http://via.placeholder.com/300x300"
+              alt="Fertilizer user"
+            />
+          </div>
+          <h4 className="text-style-1">{title_user_2}</h4>
+          <p className="text-style-2">{content_user_2}</p>
+          <Link to="/get-fertilizer">
+            <Button className="button-style-one">Details</Button>
+          </Link>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default CallToAction;
