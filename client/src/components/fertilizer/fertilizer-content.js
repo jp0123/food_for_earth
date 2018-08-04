c587afb8f48df8b78f98c2542fa26d6309a8f3f6
Joseph Park <joseph.thomas.park@gmail.com>
2018-08-04 23:11:34 +1000

    Update Fertilizer component

--- client/src/components/fertilizer/fertilizer-content.js
+++ client/src/components/fertilizer/fertilizer-content.js
@@ -1,10 +1,41 @@
 import React from "react";
-import { Container } from "reactstrap";
+import { Button, Row, Col, Container } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const FertilizerContent = () => {
+  const title = "Organic Fertilizer For All";
+  const content_1 =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, explicabo tempore quo reiciendis facere recusandae sint veniam itaque a culpa rem, architecto esse delectus, blanditiis quia nemo vero nostrum molestiae.";
+
+  const content_2 = "Want to buy some instead? Visit our store.";
+
   return (
-    <Container>
-      <h1>Fertilizer content</h1>
+    <Container className="upper-space">
+      <Row>
+        <Col sm="12" md="6" className="justify-container">
+          <div className="info-container">
+            <div>
+              <h3>{title}</h3>
+              <p className="text-justify">{content_1}</p>
+              <p className="text-justify">{content_2}</p>
+              <Link to="/store">
+                <div className="button-container">
+                  <Button className="button-style-one">Shop now</Button>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </Col>
+        <Col sm="12" md="6">
+          <div class="images-container">
+            <img
+              className="img-fluid info-images"
+              src="http://via.placeholder.com/300x300"
+              alt="Fertilizer"
+            />
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 };
